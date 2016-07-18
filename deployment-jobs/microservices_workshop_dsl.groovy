a9c8984112dfd1c9f79995e6f2add0bbd33a1a92
Ricardo Hernandez <ricardohmon@gmail.com>
2016-07-18 20:49:33 +0200

    - Renamed Ansible role - Added DSL Job for App deployment using Ansible. - Support Jenkins Job Workspace variable passing. - Fixed Ansible and Vagrant folder permissions access to enable it for Jenkins. - Specified VM memory in Vagrantfile - Removed unnecesary instructions.

---
+++ deployment-jobs/microservices_workshop_dsl.groovy
@@ -0,0 +1,47 @@
+job('vertx-microservices-workshop-job') {
+    jdk('JDK8')
+    scm {
+        git('git://github.com/cescoffier/vertx-microservices-workshop.git')
+    }
+    triggers {
+        scm('*/15 * * * *')
+    }
+    steps {
+        def mvnInst = 'M3.3.9'
+    	maven {
+    		goals('-e clean package install')
+            mavenInstallation(mvnInst)
+            localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
+    	}
+        maven {
+            goals('-e package')
+            mavenInstallation(mvnInst)
+            rootPOM('solution/quote-generator/pom.xml')
+            localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
+        }
+        maven {
+            goals('-e package')
+            mavenInstallation(mvnInst)
+            rootPOM('solution/portfolio-service/pom.xml')
+            localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
+        }
+        maven {
+            goals('-e package')
+            mavenInstallation(mvnInst)
+            rootPOM('solution/audit-service/pom.xml')
+            localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
+        }
+        maven {
+            goals('-e package')
+            mavenInstallation(mvnInst)
+            rootPOM('solution/compulsive-traders/pom.xml')
+            localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
+        }
+
+      	ansiblePlaybook('/ansible/playbook.yml') {
+        	inventoryPath('/ansible/hosts')
+	        ansibleName('Ansible2.0')
+	        credentialsId('vagrant-key')
+	    }
+    }
+}
